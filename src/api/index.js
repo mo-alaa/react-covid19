a1e322bcac91f1431a34399ef7ed2bebcbb68c3f
mo-alaa <mouhammedalaa@gmail.com>
2021-05-21 13:11:48 +0200

    remove useless comments

--- src/api/index.js
+++ src/api/index.js
@@ -49,32 +49,6 @@ export const fetchSummaryData = async (country) => {
                         })
                     }
                 }
-                //hashmap to array (not the best solution, but to handle the data from server)
-                //data is accummulative, but we dont need this.  We need data for each day only
-                // let dataList = [];
-                // let keys = Array.from(chartHashmap.keys());
-                // keys.forEach((key) => {
-                //     let value = chartHashmap.get(key);
-                //     dataList.push({ Date: key, Confirmed: value.Confirmed, Recovered: value.Recovered, Deaths: value.Deaths });
-                // })
-
-                // for (let i = 0; i < dataList.length - 1; i++) {
-                //     if (dataList[i + 1].Confirmed > 0) {
-                //         dataList[i + 1].Confirmed = dataList[i + 1].Confirmed - dataList[i].Confirmed;
-                //     }
-                //     if (dataList[i + 1].Recovered > 0) {
-                //         dataList[i + 1].Recovered = dataList[i + 1].Recovered - dataList[i].Recovered;
-                //     }
-                //     if (dataList[i + 1].Deaths > 0) {
-                //         dataList[i + 1].Deaths = dataList[i + 1].Deaths - dataList[i].Deaths;
-                //     }
-                // }
-                // for (let i = 0; i < chartHashmap.size; i++) {
-                //     console.log(chartHashmap[i]);
-                //     // if(i!==chartHashmap.size -1 && chartHashmap[i+1].value.Confirmed !== 0 ){
-                //     //     chartHashmap[i+1].value.Confirmed = chartHashmap[i+1].value.Confirmed - chartHashmap[i].value.Confirmed
-                //     // }
-                // }
 
                 return {
                     Confirmed: sumCases(latestData, 'Confirmed'),
@@ -98,10 +72,6 @@ export const fetchSummaryData = async (country) => {
 
         const summaryData = await axios.get(`https://covid19.mathdro.id/api`);
         const { confirmed, recovered, deaths, lastUpdate } = summaryData.data;
-        // console.log(data);
-        // return {
-        //     Confirmed: TotalConfirmed, Recovered: TotalRecovered, Deaths: TotalDeaths, LatestUpdate: new Date()
-        // };
 
         const { data } = await axios.get(`https://covid19.mathdro.id/api/daily`);
         const DailyData = data.map((dailyData) => ({
@@ -128,40 +98,6 @@ let sumCases = (arr, key) => {
     return arr.reduce((a, b) => a + b[key], 0)
 }
 
-export const fetchData = async (country) => {
-    // let changeableUrl = url;
-    // if (country) {
-    //     try {
-    //         changeableUrl = `https://api.covid19api.com/dayone/country/${country}`;
-    //         const { data } = await axios.get(changeableUrl);
-    //         return data;
-    //     }
-    //     catch (error) {
-
-    //     }
-    // }
-    // else {
-    //     try {
-    //         const { data: { Global: { TotalConfirmed, TotalRecovered, TotalDeaths } } } = await axios.get(`https://api.covid19api.com/summary`);
-    //         return { TotalConfirmed, TotalRecovered, TotalDeaths };
-    //     } catch (error) {
-
-    //     }
-    // }
-
-}
-// export const fetchData = async (country) => {
-//     let changeableUrl = url;
-//     if (country) {
-//         changeableUrl = `${url}/countries/${country}`;
-//     }
-//     try {
-//         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableUrl);
-//         return { confirmed, recovered, deaths, lastUpdate };
-//     } catch (error) {
-
-//     }
-// }
 
 export const fetchDailyData = async () => {
     try {
@@ -188,12 +124,3 @@ export const fetchCountries = async () => {
 
     }
 }
-// export const fetchCountries = async () => {
-//     try {
-
-//         const { data: { countries } } = await axios.get(`${url}/countries`);
-//         return countries.map((country) => country.name);
-//     } catch (err) {
-
-//     }
-// }
